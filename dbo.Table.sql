795b24f54fb25bce39698c66c0fc3bc9a40a057b
SamreenRafiq-27 <68330353+SamreenRafiq-27@users.noreply.github.com>
2020-07-16 13:13:40 +0500

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Employedetails]
+(
+	[Id] INT NOT NULL identity(1,1)PRIMARY KEY, 
+    [Name] VARCHAR(255) NULL, 
+    [Surname] VARCHAR(255) NULL, 
+    [Nic] VARCHAR(255) NULL, 
+    [Salary] VARCHAR(255) NULL, 
+    [Contact] VARCHAR(255) NULL, 
+    [Email] VARCHAR(255) NULL, 
+    [Designation] VARCHAR(255) NULL, 
+    [Residetialaddress] VARCHAR(255) NULL, 
+    [Gender] VARCHAR(255) NULL, 
+    [Religion] VARCHAR(255) NULL, 
+    [Datofbirth] VARCHAR(255) NULL, 
+    [Martialstatus] VARCHAR(255) NULL, 
+    [Timingsshift] DATETIME NULL
+)
